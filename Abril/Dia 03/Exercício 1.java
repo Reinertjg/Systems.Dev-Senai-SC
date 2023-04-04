bb2b4a7fbee21dbf997d40194fcf90d73b333a80
João G <127997588+Reinertjg@users.noreply.github.com>
2023-04-04 09:17:37 -0300

    Update Exercício 1.java

--- Abril/Dia 03/Exercício 1.java
+++ Abril/Dia 03/Exercício 1.java
@@ -40,30 +40,30 @@ public class Exercicio3 {
 
 		JOptionPane.showMessageDialog(null, "A quantidade de numeros positivos é: " + QuantidadePositiva +
 				"\nA quantidade de numeros negativos é: " + QuantidadeNegativa);
-		// Situação problema:
-		//
-		// Crie um algoritmo que realize a entrada de 5 números.
-		//
-		// Processe as entradas e contabilize quando números são negativos e quantos
-		// números são positivos.
-		//
-		// Ao final imprima a quantidade de números de cada tipo.
-		//
-		// ----------------------------------------------------------------------------
-		//
-		// Informe o numero 1: ___
-		//
-		// Informe o numero 2: ___
-		//
-		// Informe o numero 3: ___
-		//
-		// Informe o numero 4: ___
-		//
-		// Informe o numero 5: ___
-		//
-		// Quantidade de números negativos: 3
-		//
-		// Quantidade de números positivos: 2
+// Situação problema:
+//
+// Crie um algoritmo que realize a entrada de 5 números.
+//
+// Processe as entradas e contabilize quando números são negativos e quantos
+// números são positivos.
+//
+// Ao final imprima a quantidade de números de cada tipo.
+//
+// ----------------------------------------------------------------------------
+//
+// Informe o numero 1: ___
+//
+// Informe o numero 2: ___
+//
+// Informe o numero 3: ___
+//
+// Informe o numero 4: ___
+//
+// Informe o numero 5: ___
+//
+// Quantidade de números negativos: 3
+//
+// Quantidade de números positivos: 2
 	}
 
 }
