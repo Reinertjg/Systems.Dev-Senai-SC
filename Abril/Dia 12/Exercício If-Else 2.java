6266b8d40bb9e71168d53f96c05f99b4cc464c0b
João G <127997588+Reinertjg@users.noreply.github.com>
2023-04-19 19:42:54 -0300

    Update Exercício If-Else 2.java

--- Abril/Dia 12/Exercício If-Else 2.java
+++ Abril/Dia 12/Exercício If-Else 2.java
@@ -4,18 +4,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         byte idade = 0;
+        String mensagem = ""
 
         idade = Byte.parseByte(JOptionPane.showInputDialog("Informe sua idade!!"));
 
         if (idade < 16) {
-            JOptionPane.showMessageDialog(null, "Não Eleitor");
+            mensagem = ("Não Eleitor");
         }
         else if(idade >= 18 && idade <= 65){
-            JOptionPane.showMessageDialog(null, "Eleitor Obrigatório");
+            mensagem = ("Eleitor Obrigatório");
         }
         else{
-            JOptionPane.showMessageDialog(null, "Eleitor Facultativo");
+            mensagem = ("Eleitor Facultativo");
         }
+
+        JOptionPane.showMessageDialog(null, mensagem)
 // 2 - Situação problema: Verificar a classe eleitoral de uma pessoa. Ler a idade. Verificar se a idade é 
 // menor que 16(não eleitor), entre 18 e 65 anos (eleitor obrigatório) ou idade igual a 16 ou 17 ou
 //     maior que 65 (eleitor facultativo). Informar a idade e mostrar na tela em qual grupo a idade se enquadra.
