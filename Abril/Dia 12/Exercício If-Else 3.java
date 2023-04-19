c9bf7b033245d4fa5f141802431627dc8b60540e
João G <127997588+Reinertjg@users.noreply.github.com>
2023-04-19 20:50:12 -0300

    Update Exercício If-Else 3.java

--- Abril/Dia 12/Exercício If-Else 3.java
+++ Abril/Dia 12/Exercício If-Else 3.java
@@ -4,18 +4,21 @@ public class Exercício3 {
     public static void main(String[] args) throws Exception {
         
         byte idade = 0;
+        String mensage = ""
 
         idade = Byte.parseByte(JOptionPane.showInputDialog("Informe sua idade!!"));
 
         if (idade < 18) {
-            JOptionPane.showMessageDialog(null, "Menor de Idade");
+            mensage = ("Menor de Idade");
         }
         else if(idade >= 18 && idade <= 64){
-            JOptionPane.showMessageDialog(null, "Maior de idade");
+            mensage = ("Maior de idade");
         }
         else{
-            JOptionPane.showMessageDialog(null, "Idoso");
+            mensage = ("Idoso");
         }
+        
+        JOptionPane.showMessageDialog(null, mensage)
 //3 - Situação problema: Verificar se a idade é menor que 18 (menor de idade), entre 18 e 64 anos
 //(maior de idade) ou maior ou igual a 65 anos (idoso). Informar a idade e mostrar na tela em que 
 //grupo a idade se enquadra.
