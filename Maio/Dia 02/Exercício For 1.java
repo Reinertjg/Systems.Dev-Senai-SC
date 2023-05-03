fb918ea8dba845bc0f87bf777b722b8599bcbcf6
MatheusDeltt <128099889+MatheusDeltt@users.noreply.github.com>
2023-05-02 21:19:03 -0300

    Update Exercício For 1.java

--- Maio/Dia 02/Exercício For 1.java
+++ Maio/Dia 02/Exercício For 1.java
@@ -1,1 +1,24 @@
-a
+
+package atividadefor;
+
+import javax.swing.JOptionPane;
+
+
+public class AtividadeFor {
+
+   
+    public static void main(String[] args) {
+        
+       
+        int i = 1;
+        String mensagem = "";
+        
+        for (i =1; i<=10; i++){
+            System.out.println(i);
+            
+        }
+        
+        
+    }
+    
+}
