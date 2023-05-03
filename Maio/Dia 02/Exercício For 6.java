9c715a363665a09abe595325ccfb08373f08156f
MatheusDeltt <128099889+MatheusDeltt@users.noreply.github.com>
2023-05-02 21:21:33 -0300

    Update Exercício For 6.java

--- Maio/Dia 02/Exercício For 6.java
+++ Maio/Dia 02/Exercício For 6.java
@@ -1,1 +1,17 @@
 
+package atividadefor;
+
+
+public class Atividade5 {
+    public static void main(String[] args) {
+        
+        int i = 1;
+        String mensagem = "";
+        
+        for (i =2; i<=100; i++){
+            System.out.println(i);
+            i++;
+        
+        }
+    }
+}
