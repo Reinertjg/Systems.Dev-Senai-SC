c7057bf7a04f48f863647157143a29c094b2a887
MatheusDeltt <128099889+MatheusDeltt@users.noreply.github.com>
2023-05-02 21:20:51 -0300

    Update Exercício For 4.java

--- Maio/Dia 02/Exercício For 4.java
+++ Maio/Dia 02/Exercício For 4.java
@@ -1,1 +1,26 @@
-a
+package atividadefor;
+
+import javax.swing.JOptionPane;
+
+
+
+public class Atividade4For {
+
+    public static void main(String[] args) {
+
+        
+        int numero;
+        
+        
+       numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro para calcular a tabuada: "));
+        
+        
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numero + " x " + i + " = " + (numero * i));
+        }
+        
+        
+
+        
+    }
+}
