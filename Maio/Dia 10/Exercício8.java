60b617795afab3921b045a36c46b71346ca440e4
João G <127997588+Reinertjg@users.noreply.github.com>
2023-05-17 22:11:49 -0300

    Update Exercício8.java

--- Maio/Dia 10/Exercício8.java
+++ Maio/Dia 10/Exercício8.java
@@ -6,18 +6,18 @@ public class Exercício8 {
 
     public static void main(String[] args) {
 
-        int cc = 1;
         double maiorNumero = 0.0;
         double numero = 0.0;
 
-        do {
+        for(int cc=1; cc!=51; cc++)
+        {
             numero = Double.parseDouble(JOptionPane.showInputDialog("Informe 50 numeros (" + cc + "/50)"));
 
             if (numero > maiorNumero) {
                 maiorNumero = numero;
             }
-            cc++;
-        } while (cc != 51);
+            
+        }
 
         JOptionPane.showMessageDialog(null, "O maior numero é: " + maiorNumero);
     }
