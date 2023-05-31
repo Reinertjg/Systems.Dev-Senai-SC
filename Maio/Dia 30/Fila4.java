0b5e5905252d72086697916e0a31f0c01f41384a
João G <127997588+Reinertjg@users.noreply.github.com>
2023-05-30 21:48:04 -0300

    Create Fila4.java

---
+++ Maio/Dia 30/Fila4.java
@@ -0,0 +1,16 @@
+package javaapplication1;
+
+public class Fila4 {
+
+    public static void main(String[] args) {
+
+        int cc = 1;
+
+        for (int ii = 2; ii <= 7; ii++) {
+
+            cc++;
+            System.out.println(ii);
+            System.out.println(ii + cc);
+        }
+    }
+}
