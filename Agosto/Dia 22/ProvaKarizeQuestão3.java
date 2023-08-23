619b2296da5372cc488968e40354182e2c310563
João G <127997588+Reinertjg@users.noreply.github.com>
2023-08-23 18:42:52 -0300

    Create ProvaKarizeQuestão3.java

---
+++ Agosto/Dia 22/ProvaKarizeQuestão3.java
@@ -0,0 +1,12 @@
+package Prova;
+
+public class Questao3 {
+
+    public static void main(String[] args) {
+        
+        for(int ii = 1000; ii <= 2000; ii++){
+            System.out.println("Contagem - " + ii);
+        }
+        
+    }
+}
