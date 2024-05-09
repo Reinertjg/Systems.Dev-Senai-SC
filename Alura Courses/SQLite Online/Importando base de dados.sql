08b674380eb7640a8aae6e1216fe6ba807c6aaf3
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-05-09 07:30:58 -0300

    commit git

---
+++ Alura Courses/SQLite Online/Importando base de dados.sql
@@ -0,0 +1,9 @@
+SELECT * FROM nome da tabela;
+
+SELECT * FROM nome da tabela WHERE nome da coluna = dado que desejamos consultar; 
+
+SELECT DISTINCT nome da coluna FROM nome da tabela; 
+
+SELECT DISTINCT nome_do_fornecedor, data_de_início
+from tabelafornecedores
+WHERE data_de_início > '2020-01-01'
