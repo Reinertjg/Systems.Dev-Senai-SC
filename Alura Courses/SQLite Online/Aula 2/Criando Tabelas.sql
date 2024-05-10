1721539839e9b47de5d8adc955b980f0f240d1f9
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-05-09 21:39:52 -0300

    commit git

--- Alura Courses/SQLite Online/Aula 2/Criando Tabelas.sql
+++ Alura Courses/SQLite Online/Aula 2/Criando Tabelas.sql
@@ -1,7 +1,18 @@
 CREATE TABLE tabelaclientes (
-  	ID_Cliente INT PRIMARY KEY,
+  	ID_Cliente INT PRIMARY KEY, /* Nome da coluna, Tipo de dado */
   	Nome_Cliente VARCHAR (250),
 	Informacoes_de_Contato VARCHAR(250)
 	);
-/* Nome da coluna, Tipo de dado */
+/*Cria uma tabela*/
+
+DROP tipo_do_objeto nome_do_objeto;
+DROP TABLE tabelaclientes;
+DROP DATABASE Colegio_São_Paulo;
+DROP SCHEMA Turno_da_manhã;
+/*Exclui uma tabela*/
+
+ALTER TABLE nome_da_tabela Add nome_da_coluna tipo_de_dado;
+ALTER TABLE tabelaclientes Add Endereço_Cliente VARCHAR(255);
+ALTER TABLE estudantes Add idade INT;
+/*Adiciona uma coluna "Endereço_Cliente", Tipo de dado: VARCHAR, na tabela "tabelaclientes"*/
 
