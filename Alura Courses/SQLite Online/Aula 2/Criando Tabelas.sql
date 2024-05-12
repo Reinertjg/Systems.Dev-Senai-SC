b766a6562d077bb479cd5151e075184e15608798
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-05-12 08:12:25 -0500

    commit git

--- Alura Courses/SQLite Online/Aula 2/Criando Tabelas.sql
+++ Alura Courses/SQLite Online/Aula 2/Criando Tabelas.sql
@@ -28,3 +28,25 @@ CREATE TABLE tabelaprodutos (
   FOREIGN KEY (Categoria) REFERENCES tabelacategorias (id_categoria),/*Chaves Estrangeiras*/
   FOREIGN KEY (Fornecedor) REFERENCES tabelafornecedores (id) /*Chaves Estrangeiras*/
   );
+
+CREATE TABLE tabelapedidos (
+  ID_pedido INT PRIMARY KEY,
+  Data_Do_Pedido DATE,
+  Status VARCHAR(50),
+  Total_Do_Pedido DECIMAL(10, 2),
+  Cliente INT,
+  Data_De_Envio_Estimada DATE,
+  FOREIGN KEY (cliente) REFERENCES tabelaclientes(id_cliente)
+ );
+ 
+ CREATE TABLE tabelapedidosgold (
+ ID_pedido_gold INT PRIMARY KEY,
+ Data_Do_Pedido_gold DATE,
+ Status_gold VARCHAR(50),
+ Total_Do_Pedido_gold DECIMAL(10, 2),
+ Cliente_gold INT,
+ Data_De_Envio_Estimada_gold DATE,
+ FOREIGN KEY (cliente_gold) REFERENCES tabelaclientes(id_cliente)
+);
+
+
