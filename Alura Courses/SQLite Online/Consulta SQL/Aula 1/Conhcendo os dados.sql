ab2dbf3b36a204a168428edd0383171283427b1e
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-05-23 17:14:38 -0300

    commit git

--- Alura Courses/SQLite Online/Consulta SQL/Aula 1/Conhcendo os dados.sql
+++ Alura Courses/SQLite Online/Consulta SQL/Aula 1/Conhcendo os dados.sql
@@ -1,3 +1,13 @@
 SELECT * FROM HistoricoEmprego
 ORDER BY salario DESC
 LIMIT 5;
+
+SELECT * FROM HistoricoEmprego 
+WHERE datatermino IS NULL
+ORDER BY salario DESC
+limit 5;
+
+SELECT * FROM HistoricoEmprego 
+WHERE datatermino NOT NULL
+ORDER BY salario DESC
+limit 5;
