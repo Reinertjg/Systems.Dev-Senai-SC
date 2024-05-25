0fee4d4b4384c2dbd442e1960081192adbd366b4
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-05-25 11:27:38 -0300

    commit git

---
+++ Alura Courses/SQLite Online/Consulta SQL/Aula 2/Utilizando Operadores.sql
@@ -0,0 +1,32 @@
+
+/* Utilizando o LIKE */
+SELECT * FROM Treinamento
+WHERE curso LIKE '%realizar%';
+
+SELECT * FROM Treinamento
+WHERE curso LIKE 'O poder%';
+
+SELECT * FROM Colaboradores
+WHERE nome LIKE 'Gustavo%';
+
+
+/* Utilizando AND, OR, IN E NOT */
+SELECT * FROM HistoricoEmprego
+WHERE cargo = 'Professor' AND
+datatermino NOTNULL;
+
+SELECT * FROM HistoricoEmprego
+WHERE cargo = 'Oftalmologista' OR 
+cargo = 'Dermatologista';
+
+SELECT * FROM HistoricoEmprego
+WHERE cargo IN ('Oftalmologista', 'Dermatologista', 'Professor');
+
+SELECT * FROM HistoricoEmprego
+WHERE cargo NOT IN ('Oftalmologista', 'Dermatologista', 'Professor');
+
+
+/* Utilizando diversos operadores */
+SELECT * FROM Treinamento
+WHERE (curso like 'O Direito%' AND instituicao = 'da Rocha')
+OR (curso LIKE 'O conforto%' AND instituicao = 'das Neves');
