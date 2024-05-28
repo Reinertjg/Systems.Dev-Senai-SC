d9d3d591053571b24158ca25277f9c17a6dc1e7e
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-05-27 21:43:48 -0300

    commit git

--- Alura Courses/SQLite Online/Consulta SQL/Aula 3/Funções de Agregação.sql
+++ Alura Courses/SQLite Online/Consulta SQL/Aula 3/Funções de Agregação.sql
@@ -6,6 +6,7 @@ SELECT mes, MIN(faturamento_bruto) FROM faturamento;/*Exibe o mes com minimo fat
 SELECT SUM(numero_novos_clientes) AS 'Novos Clientes  2023' FROM faturamento
 WHERE mes like '%2023'; /*Exibe a soma de clientes novos no ano de 2023*/
 
+
 /* Funções de Agregação - COUNT E AVG*/
 SELECT AVG(despesas) FROM faturamento; /* Pega a média da coluna despesas*/
 
@@ -14,3 +15,11 @@ WHERE datatermino NOT NULL; /* Conta todas as linhas da coluna datatermino, onde
 
 SELECT COUNT(*) FROM Licencas 
 WHERE tipolicenca = 'férias'; /* Conta todas as linhas da coluna tipolicenca, onde é = 'férias' */
+
+
+/* Cláusula - GROUP BY */
+SELECT parentesco FROM Dependentes
+GROUP BY parentesco; /* Busca apenas os grupos da coluna */
+
+SELECT parentesco COUNT(*) FROM Dependentes
+GROUP BY parentesco; /* Busca apenas os grupos da coluna, junto a quantidade de cada grupo */
