10dccf350f55e55b4887f71412a00899f779a63d
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-05-29 21:43:55 -0300

    commit git

--- Alura Courses/SQLite Online/Consulta SQL/Aula 4/Utilizando Funções.sql
+++ Alura Courses/SQLite Online/Consulta SQL/Aula 4/Utilizando Funções.sql
@@ -4,3 +4,9 @@ SELECT nome, LENGTH(cpf) qtd /* LENGTH conta qunatas caracteres tem no campo esp
 FROM Colaboradores
 WHERE qtd = 11;
 
+SELECT ('A pessoa colaboradora ' || nome || ' de cpf ' || cpf || ' possui o seguinte endereço: ' || endereco)
+as Texto 
+FROM Colaboradores /* Cria um texto concatenado 
+Exemplo: A pessoa colaboradora Dr. Cauê da Conceição de cpf 24657139061 possui o seguinte endereço: Recanto Isadora Nunes/ MS
+*/
+/* UPPER ou LOWER, deixam as letras maiuscula e minuscula */ 
