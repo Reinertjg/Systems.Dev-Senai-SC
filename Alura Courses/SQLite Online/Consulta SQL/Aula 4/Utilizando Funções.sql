f4af667700532b994f14ee7e2117dcda0c48db68
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-06-06 17:46:05 -0300

    commit git

--- Alura Courses/SQLite Online/Consulta SQL/Aula 4/Utilizando Funções.sql
+++ Alura Courses/SQLite Online/Consulta SQL/Aula 4/Utilizando Funções.sql
@@ -20,9 +20,21 @@ SELECT ID_colaborador, JULIANDAY (datatermino) - JULIANDAY (datacontratacao)
 FROM HistoriocoEmprego
 WHERE datatermino IS NOT NULL;
 
+
 /* Utilizando Funções Numéricas */
 /* A espressão ROUND arredonda uma quantidade de casas especifica após a virgula, neste caso determinamos 2 casas após a virgula */
 SELECT AVG(faturamento_bruto), ROUND (AVG(faturamento_bruto)),2) FROM faturamento;
 
 /* A expressão CEIL arrebonda o valor numero para um interio maior, ja a expressão FLOOR arrendonda para um inteiro menor */
 SELECT CEIL(faturamento_bruto),  FLOOR(despesas) FROM faturamento;
+
+
+/* Utilizando Funções de Conversão */
+SELECT (' O faturamento bruto médio foi ' || CAST(ROUND (AVG(faturamento_bruto),2) AS TEXT)) AS "Media Faturamento Bruto"
+FROM faturamento;
+/*  
+    "CAST"  Converte um valor numero em string
+    "ROUND" Arredonda uma quantidade de casas especifica após a virgula 
+    "AVG"   Cria a media da coluna
+*/
+
