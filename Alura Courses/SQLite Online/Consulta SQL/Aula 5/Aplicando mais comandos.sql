8fe59d90f5e8b44b7283a8aa84868fb88c584b48
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-06-09 17:40:00 -0300

    COMMIT GIT

--- Alura Courses/SQLite Online/Consulta SQL/Aula 5/Aplicando mais comandos.sql
+++ Alura Courses/SQLite Online/Consulta SQL/Aula 5/Aplicando mais comandos.sql
@@ -1,7 +1,10 @@
+
+/* EXpressao CASE */
+/* A expressao CASE cria condições para a consulta */
 SELECT id_colaborador, CARGO, salario,
 CASE
 WHEN salario < 3000 THEN 'Baixo'
-WHEN salario BETWEEN 3000 AND 6000 then 'Medio'
+WHEN salario BETWEEN 3000 AND 6000 THEN 'Medio'
 Else 'alto'
 END AS categoria_salario
 from HistoricoEmprego;
