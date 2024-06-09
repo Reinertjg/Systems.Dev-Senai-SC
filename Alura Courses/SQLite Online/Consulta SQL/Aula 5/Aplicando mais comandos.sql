992dd8caa9a61c41ceea7f75ab71352fc7f9cd17
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-06-09 17:50:07 -0300

    commit git

--- Alura Courses/SQLite Online/Consulta SQL/Aula 5/Aplicando mais comandos.sql
+++ Alura Courses/SQLite Online/Consulta SQL/Aula 5/Aplicando mais comandos.sql
@@ -1,10 +1,11 @@
 
 /* EXpressao CASE */
-/* A expressao CASE cria condições para a consulta */
-SELECT id_colaborador, CARGO, salario,
-CASE
-WHEN salario < 3000 THEN 'Baixo'
-WHEN salario BETWEEN 3000 AND 6000 THEN 'Medio'
-Else 'alto'
-END AS categoria_salario
+/* A expressao CASE cria condições para a consulta, junto criando uma nova coluna */
+SELECT id_colaborador, cargo, salario,
+    CASE
+        WHEN salario < 3000 THEN 'Baixo'
+        WHEN salario BETWEEN 3000 AND 6000 THEN 'Medio'
+        Else 'alto'
+    END AS categoria_salario
 from HistoricoEmprego;
+
---
+++ Alura Courses/SQLite Online/Consulta SQL/Aula 5/Clausula CASE.md
@@ -0,0 +1,10 @@
+**Estrutura Básica**
+-----------------
+A cláusula CASE tem duas formas principais: a forma simples e a forma pesquisada.     
+  **1. Forma Simples:**     
+    `CASE expressao`
+        `WHEN valor1 THEN resultado1`
+        `WHEN valor2 THEN resultado2`
+        `...`
+        `ELSE resultado_padrao`
+    `END`
