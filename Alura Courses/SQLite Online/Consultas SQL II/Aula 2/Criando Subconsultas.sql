56d20810b6765b943421a596d54008fb70082b44
João Guilherme <127997588+Reinertjg@users.noreply.github.com>
2024-08-03 00:41:14 -0300

    Commti Git Consultas II

--- Alura Courses/SQLite Online/Consultas SQL II/Aula 2/Criando Subconsultas.sql
+++ Alura Courses/SQLite Online/Consultas SQL II/Aula 2/Criando Subconsultas.sql
@@ -1,1 +1,29 @@
-a
+
+-- Unindo tabelas com o UNION
+
+-- Sintaxe: 
+-- SELECT coluna1, coluna2, coluna3 FROM tabela1
+-- UNION
+-- SELECT coluna1, coluna2, coluna3 FROM tabela2
+
+-- O UNION unifica duas tabelas, mas remove valores duplicados.
+
+-- Exemplo:
+SELECT rua, bairro, cidade, estado, cep FROM colaboradores
+UNION
+SELECT rua, bairro, cidade, estado, cep FROM fornecedores;
+
+
+-- Unindo tabelas com o UNION ALL
+
+-- Sintaxe: 
+-- SELECT coluna1, coluna2, coluna3 FROM tabela1
+-- UNION ALL
+-- SELECT coluna1, coluna2, coluna3 FROM tabela2
+
+-- O UNION ALL unifica duas tabelas e mantém todos os valores, incluindo duplicados.
+
+-- Exemplo:
+SELECT rua, bairro, cidade, estado, cep FROM colaboradores
+UNION ALL
+SELECT rua, bairro, cidade, estado, cep FROM fornecedores;
